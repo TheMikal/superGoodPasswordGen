c4a8ba81cc0ebc6f4d58e2e52196fe503d1c516a
TheMikal <131578548+TheMikal@users.noreply.github.com>
2023-07-15 18:26:03 -0400

    Final code cleanup and README finished

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-superGoodPasswordGen
+# superGoodPasswordGen
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- What was your motivation? 
+- I wanted to create a simple applications that prompts the user for input, takes the choices the user made and generate a password based on those choices when the button on the webpage is clicked.
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.") 
+- I wanted an easy to use password generator that generated a password based on my choices.
+- What problem does it solve? 
+- This solves the issue of trying to create your own strong passwords so you don't use the same password you've been using since middle-school.
+- What did you learn? 
+- Even smaller, simple projects have their own issues and unforeseen problems that you need to work through
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+
+## Installation
+
+- To install download the attached files and open in VS code, or preferred coding application.
+
+## Usage
+
+- To use the generator simply go to the deployed webpage, or download the code and open it in your default browser. 
+- Press the generate button.
+- Answer the prompts as they show up. (in the case of the confirm prompts the 'ok' option is yes and 'cancel' is no.)
+- The webpage will then generate a password for you based on you choices!
+
+
+![alt text](assets/images/screenshot.png)
+
+
+## Credits
+
+- Credit to the [mdn web docs](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Addition_assignment) for information on addition assignment.
+
+---
+
+### Created by Michael Taraschi with ❤
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
-// Assignment Code
+// Assigns btn variable to the button on the webpage
 var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
+// Add event listener to the generate button
 generateBtn.addEventListener("click", function(event){
   event.preventDefault();
   event.stopPropagation();
@@ -17,34 +17,34 @@ function generatePassword(passwordLength, includeUppercase, specialChars, numeri
   var numericChars = '0123456789';
   var specialChars = '!@#$%^&*()';
 
-  // prompts and alerts for generators
-  // Prompt for password length
+  // Prompts and alerts for generators
+  // The prompt for password length
   var passwordLength = prompt("How long do you need it to be (8-128 characters):");
 
   while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
     passwordLength = parseInt(prompt("That is not in the spectrum, try that again. Only 8-128 characters please."))
   }
-  // Prompt for numbers 
+  // The prompt for numbers 
   var includeNum = confirm("Do you want numbers?");
 
-  // Prompt for uppercase characters
+  // The prompt for uppercase characters
   var includeUppercase = confirm("Would you like uppercase?");
 
-  // Prompt for special characters
+  // The prompt for special characters
   var includeSpecialChars = confirm("Are you feeling special? Huh? Are ya punk? (special characters?)");
 
-  /* Generate the password based on user input
-  var password = generatePassword(passwordLength, includeUppercase, includeSpecialChars, includeNum);
-  console.log(password); */
-
   // Create a character pool based on user preferences
+  // the password will always have lower case characters in the pool
   var charPool = lowercaseChars;
+  // If includes uppercase add it to the character pool
   if (includeUppercase) {
     charPool += uppercaseChars;
   }
+  // If includes special characters add it to the character pool
   if (includeSpecialChars) {
     charPool += specialChars;
   }
+  // If includes numbers add it to the character pool
   if (includeNum) {
     charPool += numericChars;
   }
@@ -55,6 +55,7 @@ function generatePassword(passwordLength, includeUppercase, specialChars, numeri
     var randomIndex = Math.floor(Math.random() * charPool.length);
     password += charPool.charAt(randomIndex);
   }
+  // Calls the function that writes the password to the webpage dynamically
   writePassword(password);
 };
 
